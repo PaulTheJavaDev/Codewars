2bfadd5170acd18d1c638139f70a3205f18c48bf
Paul <Paul-Ludwig.Simon@t-online.de>
2025-01-23 20:54:26 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,30 +2,9 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList<Integer> list = new LinkedList<>();
-
-        /*
-        list.add(6);
-        list.add(36);
-        list.add(19);
-        list.add(45);
-        System.out.print("Elements before reversing: " + list);
-        list = reverseLinkedList(list);
-        System.out.print("\nElements after reversing: " + list);
-         */
-
-        //powerFunction(2, 5);
 
         digPow(46288, 3);
-    }
-
-    public static LinkedList<Integer> reverseLinkedList(LinkedList<Integer> list) {
-        LinkedList<Integer> revLinkedList = new LinkedList<>();
-        for (int i = list.size() - 1; i >= 0; i--) {
-
-            revLinkedList.add(list.get(i));
-        }
-        return revLinkedList;
+        
     }
 
     public static long digPow(int n, int p) {
